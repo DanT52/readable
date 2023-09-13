af1c006aa885cab108e801ba357025660af91655
Daniel Basarab <dantramp52@gmail.com>
2023-09-13 22:32:24 +0000

    v1

--- assignment3.c
+++ assignment3.c
@@ -69,7 +69,3 @@ int readable(char* inputPath) {
     return count;
 }
 
-int main(int argc, char *argv[]){
-	printf("number of readable files: %d \n", readable("test_folders"));
-	return 0;
-}
---
+++ main.c
@@ -0,0 +1,4 @@
+int main(int argc, char *argv[]){
+	printf("number of readable files: %d \n", readable("test_folders"));
+	return 0;
+}
