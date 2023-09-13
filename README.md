8331a588003248263fff028f5470c7f7be9f690d
Daniel Basarab <dantramp52@gmail.com>
2023-09-13 22:30:59 +0000

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # readable
+
+count = 0
+
+open the specified directory
+
+go through files ntill null
+
+skip . and ..
+
+if file is symbolic link skip
+
+if file is a dir then run readable on it and:
+    count += readable(dir)
+
+if file is a file: check if readable and if it is count += 1
+
+return count.
+
+
--- assignment3.c
+++ assignment3.c
@@ -7,6 +7,16 @@ description:
 
 /* Includes and definitions */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+
 /**********************************************************************
 Take as input some path. Recursively count the files that are readable.
 If a file is not readable, do not count it. Return the negative value
@@ -16,6 +26,50 @@ to respond to.
 
 
 int readable(char* inputPath) {
-	//This is where you do the work. ;)
-    return 0;
+
+	DIR *dir;
+	struct dirent *ent;
+	struct stat buf;
+	char *file;
+
+	char *current = getcwd(NULL, 0);
+
+	chdir(inputPath);
+	dir = opendir(".");
+	int count = 0;
+	
+
+	while ((ent = readdir(dir)) != NULL){
+		file = ent->d_name;
+		if (!strcmp(file, ".")|| !strcmp(file, ".."))continue;
+		printf("current: %s : ", file);
+
+		lstat(file, &buf);
+
+		if (S_ISREG(buf.st_mode) && !access(file, R_OK)){
+			count++;
+			printf("reg file\n");
+
+		}
+		
+
+		else if (S_ISDIR(buf.st_mode)){
+			printf("\n \n In Folder: %s \n", file);
+			count += readable(file);
+			printf("\n");
+		}
+		
+
+		
+	}
+
+	chdir(current);
+	free(current);
+	closedir(dir);
+    return count;
+}
+
+int main(int argc, char *argv[]){
+	printf("number of readable files: %d \n", readable("test_folders"));
+	return 0;
 }
